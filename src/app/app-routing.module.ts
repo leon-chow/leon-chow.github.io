495255502b41f0dd0692eb0c3ed641429873e66e
Leon <leon.chow1@ontariotechu.net>
2020-12-30 13:21:35 -0500

    Add angular material, set up navbar skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { NavbarComponent } from './navbar/navbar.component';
+
+const routes: Routes = [
+  {
+    path: '', component: NavbarComponent
+  },
+  {
+    path: '**', pathMatch: 'full', redirectTo: ''
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
