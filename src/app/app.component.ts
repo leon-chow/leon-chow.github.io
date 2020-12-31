dd1151b80af78e3520bf495ed994fc975705ac36
Leon <leon.chow1@ontariotechu.net>
2020-12-30 21:45:48 -0500

    Add drawer styling to align correctly, add scroll event to check if sticky should appear, add mat toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'leon-chow';
+  isSticky: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    this.isSticky = window.pageYOffset >= 50;
+  }
 }
